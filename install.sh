e597554f9cd924c913b146861328c81f824977cc
Debraj-Das <debrajdas.gayatri@gmail.com>
2025-05-05 12:35:17 +0530

    alarm scripts build and install scripts start to build

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+
+# Set -euo pipefail:  Exit immediately if a command exits with a non-zero status.
+# -e: Exit if a command exits with a non-zero status.
+# -u: Treat unset variables as an error.
+# -o pipefail:  If a command in a pipeline fails, the whole pipeline fails.
+set -euo pipefail
+
+sudo apt update
+sudo apt upgrade
+
+
+# for alarm.sh and run *.mp3 audio songs
+sudo apt install mpg123
+
+
+
+exit 0
---
+++ scripts/alarm.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+
+# Set -euo pipefail:  Exit immediately if a command exits with a non-zero status.
+# -e: Exit if a command exits with a non-zero status.
+# -u: Treat unset variables as an error.
+# -o pipefail:  If a command in a pipeline fails, the whole pipeline fails.
+set -euo pipefail
+
+# song=~/Music/alarm.mp3
+
+# mpg123 -q --loop -1 $song
+
+if [ $# -lt 1 ]; then
+  echo "Time needed"
+  echo "Useage: $0 <seconds>"
+  echo "Example: $0 2"
+  exit 0
+fi
+ 
+function main(){
+	s=1
+	start=$SECONDS
+	time="$1"
+	alarm="$2"
+	loop="$3"
+
+	echo "Timer started..."
+	while [ $s -gt 0 ]
+	do
+	s="$((time - (SECONDS - start)))"
+	echo -ne "\r                   \r"
+
+	min=$(("$s"/60))
+	sec=$(("$s"%60))
+
+	if [[ min -eq 0 ]]; then
+		echo -ne "\r$s seconds left"
+	else
+		echo -ne "\r$min mins $sec secs left"
+	fi
+
+	sleep 1
+	done
+
+	echo -e "\nTimes Up"
+
+	if [ -f "$alarm" ]; then
+		mpg123 -q --loop "$loop" "$alarm" 
+	else
+		for i in `seq 1 3`; do
+		  play -n -c1 synth sin %-12 sin %-9 sin %-5 sin %-2 fade h 0.1 1 0.1 2>/dev/null
+		done
+	fi
+
+	exit 0
+}
+
+
+alarm="$HOME/.config/bash/assets/audio/alarm.mp3"
+
+time="$1"
+
+loop=-1
+if [[ $# -ge 2 ]] && [[ "$2" =~ ^[0-9]+$ ]] ; then
+	loop="$2"
+fi
+ 
+main "$time" "$alarm" "$loop"
+
+
+exit 0
