dee97bfcab86dc383f2df01921b4ac09844341e8
Debraj-Das <debrajdas.gayatri@gmail.com>
2025-05-09 11:56:51 +0530

    function add karna ka new simple method added

--- bash.sh
+++ bash.sh
@@ -1,20 +1,28 @@
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
-
 # Alias definitions.
 if [ -f ~/.config/bash/aliases.sh ]; then
-    . ~/.config/bash/aliases.sh
+	. ~/.config/bash/aliases.sh
 fi
 
 # Load the prompt configuration
 if [ -f ~/.config/bash/prompt.sh ]; then
-	 . ~/.config/bash/prompt.sh
+	. ~/.config/bash/prompt.sh
 fi
 
-## Load the input configuration
+## Load the input configuration and bind -f is Readine keybindings
 if [ -f ~/.config/bash/input.sh ]; then
-	 bind -f ~/.config/bash/input.sh
+	bind -f ~/.config/bash/input.sh
 fi
 
- export PATH+="$PATH:~/.config/bash/scripts:~/.config/bash/scripts/cp/"
+modules_dir=~/.config/bash/modules/
+# Check if the directory exists
+if [ ! -d "$modules_dir" ]; then
+	exit 0
+fi
+
+find "$modules_dir" -type f -name "*.sh" -print0 | while IFS= read -r -d $'\0' file; do
+	absolute_path=$(realpath "$file")
+	source $absolute_path
+done
